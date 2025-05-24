35a9a567f29d1c58cd3b58235304782e457bb58e
Mahamad Abdirashid Dahir <m.dahir@se22.qmul.ac.uk>
2025-05-24 05:26:25 +0100

    Started filling out WordBank

--- src/WordBank.java
+++ src/WordBank.java
@@ -1,4 +1,6 @@
+import java.io.File;
 import java.util.List;
+import java.util.Random;
 
 public class WordBank {
     private List<Word> words;
@@ -9,15 +11,32 @@ public class WordBank {
 
     private void loadWordsFromJson(String path) {
         // Parse JSON into List<Word>
+        ObjectMapper mapper = new ObjectMapper();
+
+        words = mapper.readValue(
+                new File(path),
+                new TypeReference<List<Word>>() {}
+        );
+
 
     }
 
     public Word getRandomWord() {
         // Return a random word from the list
+        Random random = new Random();
+        return words.get(random.nextInt(words.size()));
+
     }
 
     public Word getRandomWordByDifficulty(String difficulty) {
         // Return a filtered random word by difficulty
+        List<Word> filteredList = words;
+        for(int i=0; i<words.size(); i++){
+            if(((words.get(i)).getDifficulty()).equals(difficulty)){
+                filteredList.add(words.get(i));
+            }
+        }
+
     }
 
     public List<Word> getAllWords() {
